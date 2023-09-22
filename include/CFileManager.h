b37212cc8488fb08333f9024ed496664deec6223
Cherala Saiteja <cheralaSaiteja4@gmail.com>
2023-09-22 10:53:52 -0700

    new: cfm_list_files() implemented

--- include/CFileManager.h
+++ include/CFileManager.h
@@ -4,22 +4,48 @@
 #ifndef CFILEMANAGER_H
 #define CFILEMANAGER_H
 
+// path length
+#include <stdio.h>
+#define CFM_PATH_LENGTH 1024
+
+// length of file/folder name in cfm_node struct
+#define CFM_NODE_NAME_LENGTH 256
+
+// min count of subdirectory files/folders a cfm_node can have
+#define CFM_MIN_NODES_COUNT 256
+
 // Flat structure to store all file names
-struct FileInfo{
-    char name[1024];
-    int isFolder;
+struct cfm_node {
+  char name[CFM_NODE_NAME_LENGTH];
+  int is_folder;
+  struct cfm_node *nodes;
+  int count, head, tail;
 };
 
-// lists all files from root folder and all subfolders
-void ListFiles();
+/*
+ *   @brief lists and prints all files from root folder and all subfolders to
+ *  stdout
+ *
+ *   @param directory_path directory from where the search should be started
+ *   @param max_depth maximum depth a consider when searching
+ *   @param node_count address of integer to store number of files/folders found
+ *   @param out_stream can be used to print to files if needed. when passed null
+ * stdout considered
+ */
+void cfm_list_files(const char *directory_path, int max_depth, int *node_count,
+                    FILE *out_stream);
 
-// retrieves all files from root folder and all subfolders
-void ExploreDirectory();
+//
+// To be Implemented
+//
 
-// search for file[s] in retrieved files
-void SearchList();
+// // retrieves all files from root folder and all subfolders
+// void cfm_explore_directory();
 
-// sorts retrieved files
-void SortList();
+// // search for file[s] in retrieved files
+// void cfm_search_list();
+
+// // sorts retrieved files
+// void cfm_sort_list();
 
 #endif
--- src/CFileManager.c
+++ src/CFileManager.c
@@ -1,9 +1,80 @@
 #include <CFileManager.h>
+#include <dirent.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
 
-void ListFiles() {}
+void cfm_list_files(const char *directory_path, int max_depth, int *node_count,
+                    FILE *out_stream) {
 
-void ExploreDirectory() {}
+  // check if stream is null and set to stdout
+  if (out_stream == NULL) {
+    out_stream = stdout;
+  }
 
-void SearchList() {}
+  //  keeps tracks of depth for better display mechanism
+  static int current_depth;
 
-void SortList() {}
+  // to store current path
+  char full_path[CFM_PATH_LENGTH];
+
+  struct dirent *entry;
+
+  if (current_depth >= max_depth && max_depth != 0) {
+    return;
+  }
+
+  // open current directory
+  DIR *current_directory = opendir(directory_path);
+
+  // if unable to open
+  if (!current_directory) {
+    fprintf(out_stream, "Error opening directory %s: %s\n", full_path,
+            strerror(errno));
+    return;
+  }
+
+
+  while ((entry = readdir(current_directory))) {
+    // if entries are . & .. skip
+    if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+      continue;
+    }
+
+    // add entry to full path
+    snprintf(full_path, sizeof(full_path), "%s/%s", directory_path,
+             entry->d_name);
+
+    //  if null is passed to node_count it throws segfault
+    if (node_count != NULL) {
+      (*node_count)++;
+    }
+
+    // if entry is folder
+    // print to stdout
+    // update current_depth
+    if (entry->d_type == DT_DIR) {
+      fprintf(out_stream, "%s\n", full_path);
+
+      // depth incremented
+      current_depth += 1;
+      cfm_list_files(full_path, max_depth, node_count, out_stream);
+
+      // depth decremented on return
+      current_depth -= 1;
+    }
+
+    // if entry is file
+    // print to stdout
+    else if (entry->d_type == DT_REG) {
+
+      // print tabs for depth[s]
+      for (int i = 0; i < current_depth; i++) {
+        fprintf(out_stream, "    ");
+      }
+      fprintf(out_stream, "%s\n", full_path);
+    }
+  }
+
+  closedir(current_directory);
+}
